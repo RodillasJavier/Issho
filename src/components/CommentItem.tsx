388d6ed66dc270d9e2a76fe06ade5e6c1ca5f49c
Javier Rodillas <rodillasjavier@gmail.com>
2026-01-28 12:07:21 -0500

    addedstyling to comment replies

--- src/components/CommentItem.tsx
+++ src/components/CommentItem.tsx
@@ -36,6 +36,8 @@ const createReply = async (
 export const CommentItem = ({ comment, postId }: CommentItemProps) => {
   const [showReply, setShowReply] = useState<boolean>(false);
   const [replyText, setReplyText] = useState<string>("");
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(true);
+
   const { user } = useAuth();
   const queryClient = useQueryClient();
 
@@ -60,7 +62,7 @@ export const CommentItem = ({ comment, postId }: CommentItemProps) => {
   };
 
   return (
-    <div className="flex flex-col gap-2 bg-neutral-950 rounded p-2">
+    <div className="flex flex-col gap-2 bg-neutral-950 rounded py-2 px-3 border-l-2 border-l-neutral-600">
       <div className="space-y-1">
         <div className="text-sm text-neutral-600">
           {new Date(comment.created_at).toLocaleString()}
@@ -98,6 +100,52 @@ export const CommentItem = ({ comment, postId }: CommentItemProps) => {
 
           {isError && <p>Error posting reply.</p>}
         </form>
+      )}
+
+      {comment.children && comment.children.length > 0 && (
+        <div>
+          <button onClick={() => setIsCollapsed((prev) => !prev)}>
+            {isCollapsed ? (
+              <div className="flex items-center">
+                <p className="text-sm text-neutral-400">Show Replies</p>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6 text-neutral-400"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"
+                  />
+                </svg>
+              </div>
+            ) : (
+              <div className="flex items-center">
+                <p className="text-sm text-neutral-400">Hide Replies</p>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 -960 960 960"
+                  fill="currentColor"
+                  className="w-6 h-6 text-neutral-400"
+                >
+                  <path d="m296-345-56-56 240-240 240 240-56 56-184-184-184 184Z" />
+                </svg>
+              </div>
+            )}
+          </button>
+
+          {!isCollapsed && (
+            <div className="space-y-2">
+              {comment.children.map((child, key) => (
+                <CommentItem key={key} comment={child} postId={postId} />
+              ))}
+            </div>
+          )}
+        </div>
       )}
     </div>
   );

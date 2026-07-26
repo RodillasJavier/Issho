64874edd3b749bd85a947850ce436ffbb6b09e4a
Javier Rodillas <rodillasjavier@gmail.com>
2026-07-25 20:57:36 -0700

    feat: add Vercel Speed Insights for performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./context/AuthProvider.tsx";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const client = new QueryClient();
 
@@ -18,6 +19,8 @@ createRoot(document.getElementById("root")!).render(
         </Router>
       </AuthProvider>
     </QueryClientProvider>
+
     <Analytics />
+    <SpeedInsights />
   </StrictMode>
 );
